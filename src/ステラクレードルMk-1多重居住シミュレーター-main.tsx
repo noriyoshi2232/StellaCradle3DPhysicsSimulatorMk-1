bd332b7017d49352cd709a4450ab4896c762eba6
Noriyoshi Yamamoto <60803012+noriyoshi2232@users.noreply.github.com>
2025-11-21 01:53:55 +0900

    Fix incorrect import path in main.tsx

--- src/ステラクレードルMk-1多重居住シミュレーター-main.tsx
+++ src/ステラクレードルMk-1多重居住シミュレーター-main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './ステラクレードルMk-1多重居住シミュレーター“StellaCradle is a registered trademark of StellaCradle Corporation.”.tsx';
+import App from './ステラクレードルMk-1多重居住シミュレーター.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
